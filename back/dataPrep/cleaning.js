37c485c6150841a177b22b11263f689004059690
eericsm <eeric.segawa@gmail.com>
2026-05-06 12:54:06 -0300

    data preping over

--- back/dataPrep/cleaning.js
+++ back/dataPrep/cleaning.js
@@ -14,26 +14,75 @@ async function prep() {
     const raw = await fs.readFile(RAW_FILE, "utf-8");
     const data = JSON.parse(raw);
 
-    // Map the old keys from your data file to the new keys you want
-    const categoryMap = {
-        warframes: "warframe_parts",
-        warframe_sets: "warframe_sets",
-        weapons: "weapon_parts",
-        weapon_sets: "weapon_sets",
-        companions: "companion_parts",
-        companion_sets: "companion_sets",
-        arcanes: "arcanes",
-        mods: "mods"
-    };
-
-    const slugs = {};
-
-    // Loop through the map once and dynamically extract the identifiers
-    for (const [oldKey, newKey] of Object.entries(categoryMap)) {
-        if (data[oldKey]) {
-            slugs[newKey] = data[oldKey].map(i => i.url_name || i.slug);
+    function formatItem(item) {
+        if (!item) return null;
+        const obj = {
+            slug: item.slug
+        };
+        if (item.i18n && item.i18n.en && item.i18n.en.name) obj.name = item.i18n.en.name;
+        else if (item.item_name) obj.name = item.item_name;
+        
+        if (item.tags) obj.tags = item.tags;
+        if ('ducats' in item) obj.ducats = item.ducats;
+        if ('maxRank' in item) obj.max_rank = item.maxRank;
+        else if ('max_rank' in item) obj.max_rank = item.max_rank;
+        
+        if (item.i18n && item.i18n.en && item.i18n.en.thumb) {
+            obj.thumb = item.i18n.en.thumb;
+        } else if (item.thumb) {
+            obj.thumb = item.thumb;
         }
+        return obj;
     }
+
+    function groupItems(setsData, partsData) {
+        const result = {};
+
+        if (setsData) {
+            for (const item of setsData) {
+                const slug = item.slug;
+                let baseName = slug.replace(/_prime_set$/, "").replace(/_set$/, "");
+                result[baseName] = {
+                    set: formatItem(item),
+                    parts: []
+                };
+            }
+        }
+
+        if (partsData) {
+            for (const item of partsData) {
+                const slug = item.slug;
+                let baseName = slug;
+                if (slug.includes("_prime_")) {
+                    baseName = slug.split("_prime_")[0];
+                } else {
+                    let matchingBase = Object.keys(result).find(b => slug.startsWith(b + "_") || slug === b);
+                    if (matchingBase) baseName = matchingBase;
+                }
+
+                if (!result[baseName]) {
+                    result[baseName] = { set: null, parts: [] };
+                }
+                result[baseName].parts.push(formatItem(item));
+            }
+        }
+        
+        return result;
+    }
+
+    const slugs = {
+        warframes: groupItems(data.warframe_sets, data.warframes),
+        weapons: groupItems(data.weapon_sets, data.weapons),
+        companions: groupItems(data.companion_sets, data.companions),
+        arcanes: data.arcanes ? data.arcanes.reduce((acc, item) => {
+            acc[item.slug] = formatItem(item);
+            return acc;
+        }, {}) : {},
+        mods: data.mods ? data.mods.reduce((acc, item) => {
+            acc[item.slug] = formatItem(item);
+            return acc;
+        }, {}) : {}
+    };
 
     await fs.writeFile(
         SAVE_PATH,
--- back/instructions.md
+++ back/instructions.md
@@ -1,1 +1,7 @@
 1. devUtils/downloadLocally to download the data locally
+- i will need to make it so it doesnt require manual download later, or it could already come with the raw file installed and if some item is missing then the user can refresh the whole thing and it would add the new items too
+
+2. dataPrep/filterLocally to filter(shocking) the raw download file
+- the raw file comes with all items in the game, and i dont need anywhere near all the items in the game
+
+3. dataPrep/cleaning is an insane function
